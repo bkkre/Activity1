da6d1fec0cb6382ed5fa91c2fb6b9b3ec8917c46
bakjp005 <bakjp005@mymail.unisa.edu.au>
2023-03-07 18:22:15 +1030

    Completed Activity 1

--- PracticalWeek2/PracticalWeek2.cpp
+++ PracticalWeek2/PracticalWeek2.cpp
@@ -1,3 +1,6 @@
+// PracticalWeek2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 /* The following are function declarations. They _declare_ that the functions
@@ -10,25 +13,53 @@ void function1(int);
 void function2(int*);
 int main() {
 	int number = 10;
+
 	/* C1. display the contents of number to the screen */
+	printf("Value of number is: %d\n",number);
+
 	/* C2. display the address of variable number to the screen */
+	int* numPtr = NULL;
+	numPtr = &number;
+	printf("Address of number is: %p\n", numPtr);
+
 	/* C3. call function1 */
+	function1(number);
+
 	/* C4. display the contents of number to the screen */
+	printf("Value of number is: %d\n", number);
+
 	/* C5. call function2 */
+	function2(&number);
+
 	/* C6. display the contents of number to the screen */
+	printf("Value of number after function calls: %d\n", number);
+
 	return 0;
 }
 void function1(int number)
 {
 	/* C7. increment number by one */
+	number += 1;
+
 	/* C8. display the contents of number to the screen (should be 11) */
+	printf("Value of number is: %d\n", number);
+
 	/* C9. display the address of number to the screen */
+	printf("Address of number is: %p\n", &number);
 }
 void function2(int* numberPtr)
 {
 	/* C10. display the address of number to the screen (should be the same
-	address as you displayed first in main) */
-	/* C11. display the value that the pointer numberPtr points to */
-	/* C12. increment the value stored in number */
-	/* C13. display the contents of number to the screen */
+address as you displayed first in main) */
+	printf("Address of number in function2 at the start is: %p\n", numberPtr);
+
+/* C11. display the value that the pointer numberPtr points to */
+	printf("Value of number in function2 is: %d\n", *numberPtr);
+
+/* C12. increment the value stored in number */
+	*numberPtr = *numberPtr + 1;
+
+/* C13. display the contents of number to the screen */
+	printf("Value of number in function2 is : %d\n", *numberPtr);
+
 }
